79e4224c12f360fb43f00f3b869d1157e14d5af4
Michael Kilday <mike@dragonfrugal.com>
2021-04-02 12:43:20 -0400

    Auto-create titles for thumb images in lightbox
    Auto-create titles for thumb images in lightbox

--- js/init.js
+++ js/init.js
@@ -6,12 +6,23 @@ $(document).ready(function(){
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 
+
+// Lightbox options
 lightbox.option({
 		'alwaysShowNavOnTouchDevices': true,
       'fitImagesInViewport': true,
       'wrapAround': true,
       'positionFromTop': 5
 });
+
+
+// Auto-create titles for thumb images in lightbox
+$('a.full').each(function() {
+currentA = $(this);
+dataTitle = currentA.attr('data-title');
+currentA.find("img").attr('title', dataTitle);
+});
+
 
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
